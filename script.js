687cd1712c15cef714e57943bd030faaf902e76c
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2025-02-02 14:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+    function updateSizeInfo() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const sizeInfo = document.getElementById('sizeInfo').querySelector('h1');
+      sizeInfo.textContent = `Width: ${width} and Height: ${height}`;
+    }
+
+    // Event listener for resizing the window
+    window.addEventListener('resize', updateSizeInfo);
+
+    // Initial size update when the page loads
+    updateSizeInfo();
